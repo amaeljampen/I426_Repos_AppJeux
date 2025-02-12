c2bdb258d6857bd7bd84b053b9bc96b0a023abfd
Samuel Gergely <143069755+SamuelGergely@users.noreply.github.com>
2025-02-12 16:38:11 +0100

    Accueil, 12.02.2025, in progress

---
+++ Accueil/accueil.js
@@ -0,0 +1,8 @@
+// let FlappyBird = document.getElementById();
+// let 2048 = document.getElementById();
+// let Snake = document.getElementById();
+// let Woordle = document.getElementById();
+// let Demineur = document.getElementById();
+
+const list_game = ["2048.png", "FlappyBird.png", "Snake.png", "Woordle.png", "Demineur.png"]
+
